ee9624970524d6b4bbea2d816afa9aab7278f0f5
mszarlinski <mszarlinski@gmail.com>
2015-10-16 15:58:05 +0200

    Small refactor

--- src/main/java/com/example/WhoAmIController.java
+++ src/main/java/com/example/WhoAmIController.java
@@ -11,7 +11,13 @@ import java.security.Principal;
  */
 @RestController
 public class WhoAmIController {
-
+    /**
+    curl --request GET \
+      --url http://localhost:8080/whoAmI \
+      --header 'authorization: Bearer bcf11c6d-dfe7-4fb7-8213-cc521e16836d' \
+      --header 'cache-control: no-cache' \
+      --header 'postman-token: 4a36c0d0-cdca-ddd7-54ff-7c9abd8e694b'
+     */
     @PreAuthorize("authenticated")
     @RequestMapping("/whoAmI")
     public String whoAmI( final Principal principal) {
